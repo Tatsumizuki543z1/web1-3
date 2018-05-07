628ee1ab41b0e604644f0cba5eb9cbed68d1ca91
Unknown <g1627039@st.t-kougei.ac.jp>
2018-05-07 17:39:55 +0900

    第4回_メソッドとクラス

---
+++ Sample/Assets/Test.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player{
+
+	private int hp = 100;
+	private int power = 50;
+
+	public void Attack(){
+		Debug.Log (this.power + "のダメージを与えた");
+	}
+	public void Damage(int damage){
+		this.hp -= damage;
+		Debug.Log (damage + "のダメージを受けた");
+	}
+}
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Player myPlayer = new Player ();
+		myPlayer.Attack ();
+		myPlayer.Damage (30);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
